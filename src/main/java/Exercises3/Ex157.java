5454db688c45723eb005b3982b8309a9224cd655
minhtrungle <minhtrungle21599@gmail.com>
2023-03-06 16:34:03 +0700

    Update Ex 156, 157

--- src/main/java/Exercises3/Ex157.java
+++ src/main/java/Exercises3/Ex157.java
@@ -2,9 +2,9 @@ package Exercises3;
 
 import java.util.Scanner;
 
-/*156.Hãy tìm một vị trí trong mảng một chiều các số thực mà giá trị tại vị trí
-đó là giá trị “gần giá trị x nhất”.*/
-public class Ex156 {
+/*157.Cho mảng một chiều các số thực, hãy tìm đoạn [a,b] sao cho đoạn này
+chứa tất cả các giá trị trong mảng (timdoan).*/
+public class Ex157 {
     //Nhap mang
     public static void inArray(double[] arr) {
         Scanner in = new Scanner(System.in);
@@ -20,19 +20,20 @@ public class Ex156 {
             System.out.print(arr[i] + "\t");
         }
     }
-    //Tim gia tri trong mang gan gia tri x nhat
-    public static double nearNumberX(double[] arr, double x) {
-        double result = 0;
-        double nearNum = arr[0];
-
-        int temp = (int)Math.abs(nearNum - x);
+    //Tim doan a,b chua tat ca cac gia tri trong mang la doan tu so be nhat den so lon nhat
+    public static void findPara(double[] arr) {
+        // gan doan a;b la gia tru dau tien
+        double a = arr[0];
+        double b = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (nearNum > (int)Math.abs(arr[i]- x)) {
-                nearNum = (int)Math.abs(arr[i]- x);
-                result = arr[i];
+            if ( a > arr[i] ) {
+                a = (int)arr[i];
             }
+            if (b < arr[i]) {
+                b = (int)arr[i];
+            }
+            System.out.printf("Doan chua cac gia tri trong mang la: [%d; %d]", a, b);
         }
-        return result;
     }
     public static void main(String[] args) throws Exception {
         try {
@@ -40,17 +41,16 @@ public class Ex156 {
             System.out.print("Nhap kich thuoc mang vao:");
             int n = in.nextInt();
 
+
             if (n <= 1) {
                 System.out.print("Mang khong hop le");
                 System.exit(0);
             }
-
-            double x = 15;
             double[] arr = new double[n];
             inArray(arr);
             outArray(arr);
-            double result = nearNumberX(arr, x);
-            System.out.printf("\nGia tri trong mang gan gia tri x nhat la: %f", result);
+            findPara(arr);
+            //System.out.printf("\nDoan [a,b] chua tat ca cac gia tri trong mang la: %f", result);
 
         } catch (Exception e) {
             System.out.println("Exception:");
